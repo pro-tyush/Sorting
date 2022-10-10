5834580764fbf63568fa63b328b3d82efd6c7646
Jonry Simbolon <jonrysimbolon19@gmail.com>
2022-10-10 23:30:30 +0700

    adding sorting simple char with kotlin

---
+++ SortWithKotlin.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val alphabets = listOf('A','C','B','D','G','H','I','E','J','F')
+
+    println(alphabets.sorted()) // ascending
+    println(alphabets.sortedDescending()) // descending
+}
